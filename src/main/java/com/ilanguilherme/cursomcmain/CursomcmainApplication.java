62714eb8d9d0ef15683a3ef4780f81bb2472902b
guiilan <gui.tioze@gmail.com>
2021-05-07 22:03:24 -0300

    Projeto Criado

---
+++ src/main/java/com/ilanguilherme/cursomcmain/CursomcmainApplication.java
@@ -0,0 +1,13 @@
+package com.ilanguilherme.cursomcmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcmainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilanguilherme/cursomcmain/CursomcmainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilanguilherme.cursomcmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcmainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
