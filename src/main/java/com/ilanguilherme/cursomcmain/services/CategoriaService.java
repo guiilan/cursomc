41938e10a68e99ab3b8ab6e60b56a25bd666d190
guiilan <gui.tioze@gmail.com>
2021-07-03 01:52:50 -0300

    Tratamento de excecoes no endpoint de categorias

--- src/main/java/com/ilanguilherme/cursomcmain/services/CategoriaService.java
+++ src/main/java/com/ilanguilherme/cursomcmain/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ilanguilherme.cursomcmain.domain.Categoria;
 import com.ilanguilherme.cursomcmain.repositories.CategoriaRepository;
+import com.ilanguilherme.cursomcmain.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encotrando! ID: " + id + ", Tipo: " + Categoria.class.getName()
+				) );
 	}
 	
 }
