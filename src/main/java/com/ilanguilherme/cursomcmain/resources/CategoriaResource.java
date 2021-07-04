9ce403dc0bb3bca29fdf450ac0ad5027f8876109
guiilan <gui.tioze@gmail.com>
2021-07-04 00:57:15 -0300

    buscar todas categorias

--- src/main/java/com/ilanguilherme/cursomcmain/resources/CategoriaResource.java
+++ src/main/java/com/ilanguilherme/cursomcmain/resources/CategoriaResource.java
@@ -1,8 +1,11 @@
 package com.ilanguilherme.cursomcmain.resources;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,5 +26,9 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
+	@GetMapping
+	public List<Categoria> buscarTodos(){
+		return service.buscarTodos();
+	}
 }
--- src/main/java/com/ilanguilherme/cursomcmain/services/CategoriaService.java
+++ src/main/java/com/ilanguilherme/cursomcmain/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.ilanguilherme.cursomcmain.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,10 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 					"Objeto não encotrando! ID: " + id + ", Tipo: " + Categoria.class.getName()
 				) );
+	}
+	
+	public List<Categoria> buscarTodos(){
+		return repo.findAll();
 	}
 	
 }
